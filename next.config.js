14605accaa02b87e2b07e1aa426762d5e32ea59d
Lucien-kach001 <luciendk23@icloud.com>
2025-10-30 16:50:02 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** next.config.js */
 const nextConfig = {
+  // Keeping reactStrictMode: false as it was in your original file.
   reactStrictMode: false,
-  experimental: {
-    runtime: 'nodejs'
-  }
+  // *** FIX: REMOVED the entire 'experimental' block to clear the warning. ***
 };
 module.exports = nextConfig;
